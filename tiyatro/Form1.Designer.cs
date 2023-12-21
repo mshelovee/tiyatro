87cb69b2eea3b52c121ad05bae51238ac7a15f5e
halley <130781977+mshelovee@users.noreply.github.com>
2023-12-21 17:57:35 +0300

    Add files via upload

---
+++ tiyatro/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace tiyatro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cmbOyun = new System.Windows.Forms.ComboBox();
+            this.cmbSahne = new System.Windows.Forms.ComboBox();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.cbMuzikal = new System.Windows.Forms.CheckBox();
+            this.dtvBilgi = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvBilgi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Miriam CLM", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(27, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyun";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Miriam CLM", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(21, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sahne";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Miriam CLM", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(30, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tarih";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Miriam CLM", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(34, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Süre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Miriam CLM", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(32, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fiyat";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(94, 115);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(200, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // cmbOyun
+            // 
+            this.cmbOyun.FormattingEnabled = true;
+            this.cmbOyun.Items.AddRange(new object[] {
+            "Karagöz",
+            "Meddah",
+            "Kukla",
+            "Orta Oyunu",
+            "Hokkabazlık",
+            "Köy Seyirlik Oyunları"});
+            this.cmbOyun.Location = new System.Drawing.Point(93, 34);
+            this.cmbOyun.Name = "cmbOyun";
+            this.cmbOyun.Size = new System.Drawing.Size(201, 21);
+            this.cmbOyun.TabIndex = 6;
+            // 
+            // cmbSahne
+            // 
+            this.cmbSahne.FormattingEnabled = true;
+            this.cmbSahne.Items.AddRange(new object[] {
+            "KÜÇÜKÇEKMECE DT SAHNE  ",
+            "KOZYATAĞI KÜLTÜR MERKEZİ  ",
+            "ATATÜRK KÜLTÜR MERKEZİ  ",
+            "ÜSKÜDAR SAHNESİ  ",
+            "ZEYTİNBURNU SAHNESİ",
+            "SANCAKTEPE SAHNESİ  ",
+            "GARİBALDİ SAHNESİ ",
+            "FATİH BELEDİYESİ KÜLTÜR VE SANAT MERKEZİ ",
+            "EDİRNE DEVLET SAHNESİ",
+            "EDİRNE B. ATATÜRK KÜLTÜR MERKEZİ",
+            "ENEZ B. KÜLTÜR VE SANAT MERKEZİ",
+            "İPSALA B. KÜLTÜR VE SANAT MERKEZİ"});
+            this.cmbSahne.Location = new System.Drawing.Point(94, 61);
+            this.cmbSahne.Name = "cmbSahne";
+            this.cmbSahne.Size = new System.Drawing.Size(200, 21);
+            this.cmbSahne.TabIndex = 7;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(94, 88);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(200, 20);
+            this.dtpTarih.TabIndex = 8;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(94, 138);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(200, 20);
+            this.txtFiyat.TabIndex = 9;
+            // 
+            // cbMuzikal
+            // 
+            this.cbMuzikal.AutoSize = true;
+            this.cbMuzikal.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMuzikal.Location = new System.Drawing.Point(94, 164);
+            this.cbMuzikal.Name = "cbMuzikal";
+            this.cbMuzikal.Size = new System.Drawing.Size(83, 25);
+            this.cbMuzikal.TabIndex = 10;
+            this.cbMuzikal.Text = "Müzikal";
+            this.cbMuzikal.UseVisualStyleBackColor = true;
+            // 
+            // dtvBilgi
+            // 
+            this.dtvBilgi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtvBilgi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvBilgi.Location = new System.Drawing.Point(12, 218);
+            this.dtvBilgi.Name = "dtvBilgi";
+            this.dtvBilgi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtvBilgi.Size = new System.Drawing.Size(603, 186);
+            this.dtvBilgi.TabIndex = 11;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(449, 27);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(123, 48);
+            this.btnEkle.TabIndex = 12;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(449, 81);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(123, 48);
+            this.btnGuncelle.TabIndex = 13;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(449, 131);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(123, 48);
+            this.btnSil.TabIndex = 14;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(627, 416);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dtvBilgi);
+            this.Controls.Add(this.cbMuzikal);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.cmbSahne);
+            this.Controls.Add(this.cmbOyun);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "İBB Halk Tiyatrosu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvBilgi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox cmbOyun;
+        private System.Windows.Forms.ComboBox cmbSahne;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.CheckBox cbMuzikal;
+        private System.Windows.Forms.DataGridView dtvBilgi;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
